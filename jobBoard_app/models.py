3ae88d2f1237636ad767b42dbdfededf2d337fd5
Marinrp7344 <marinrp7344@gmail.com>
2024-04-02 17:17:58 -0600

    Added models and database

--- jobBoard_app/models.py
+++ jobBoard_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField("Job Description", max_length=500)
+    pay_rate = models.CharField("Pay Rate", max_length=500)
+    is_active = models.BooleanField(default = False)
+    dateMade = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', args=[str(self.id)])
+
