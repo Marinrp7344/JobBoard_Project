6c459781d1b87845a3498c196c80d46616399922
Marinrp7344 <marinrp7344@gmail.com>
2024-04-06 15:43:01 -0600

    Added image and some changes to html formatting

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Add your app name here
     'jobBoard_app',
 ]
 
@@ -119,17 +118,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
 ]
-
 MEDIA_URL = '/images/'
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
